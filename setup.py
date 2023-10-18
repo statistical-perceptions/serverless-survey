9d7575d7c5c92d6fa1eb5113a4d15cd78a96e0d8
Sarah M Brown <brownsarahm@uri.edu>
2023-10-18 15:37:23 -0400

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='serverlesssurvey',
-    version='0.3.0',
+    version='0.3.1',
     packages=['ssbuilder'],
     install_requires=[
         'Click', 'pandas', 'lxml', 'plotly', 'requests', 'html5lib',

32dfb1baf9499707e7aa40a9386edcc0582d082b
Sarah M Brown <brownsarahm@uri.edu>
2026-04-22 14:16:17 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='serverlesssurvey',
-    version='0.3.7',
+    version='0.3.8',
     packages=['ssbuilder'],
     install_requires=[
         'Click', 'pandas', 'lxml', 'plotly', 'requests', 'html5lib',
